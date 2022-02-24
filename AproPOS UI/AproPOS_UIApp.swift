6d203262555d68bd652eee0cd8712f44aa78ccad
PenTest-duck <Yoo_Ch@student.kings.edu.au>
2022-02-24 23:13:23 +1100

    Initial Commit

---
+++ AproPOS UI/AproPOS_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AproPOS_UIApp.swift
+//  AproPOS UI
+//
+//  Created by Chris Yoo on 24/2/22.
+//
+
+import SwiftUI
+
+@main
+struct AproPOS_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AproPOS UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AproPOS UI
+//
+//  Created by Chris Yoo on 24/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
