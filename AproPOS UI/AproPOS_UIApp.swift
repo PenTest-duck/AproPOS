cff0cd5a45c44523b51b4e86af643fc79b81c99b
PenTest-duck <Yoo_Ch@student.kings.edu.au>
2022-05-12 07:11:59 +1000

    Basic authentication system

--- AproPOS UI/AproPOS_UIApp.swift
+++ AproPOS UI/AproPOS_UIApp.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct AproPOS_UIApp: App {
+    
+    // Initially connect to Firebase at app startup
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             LoginView()
--- AproPOS UI/LoginView.swift
+++ AproPOS UI/LoginView.swift
@@ -6,38 +6,67 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct LoginView: View {
     
-    @State private var usernameInput = ""
-    @State private var passwordInput = ""
+    @State var email = ""
+    @State var password = ""
+    @State var isLoggedIn = false
+    @State private var failedLogin = false
     
     var body: some View {
         
-        NavigationView {
-            VStack {
-                Text("AproPOS Login")
-                    .fontWeight(.bold)
-                    .font(.system(size: 50))
-                
-                TextField(" Username", text: $usernameInput)
-                    .font(.system(size: 40))
-                    .border(.black)
-                    .padding(.bottom, 10)
-                SecureField(" Password", text: $passwordInput)
-                    .font(.system(size: 40))
-                    .border(.black)
-                
-                NavigationLink(destination: MainView()) {
-                    Image(systemName: "arrow.right.square.fill")
-                        .foregroundColor(.blue)
-                        .font(.system(size: 60))
-                        .padding(.top, 20)
+        if isLoggedIn {
+            MainView()
+        } else {
+            NavigationView {
+                VStack {
+                    Text("AproPOS Login")
+                        .fontWeight(.bold)
+                        .font(.system(size: 50))
+                    
+                    TextField(" Email", text: $email)
+                        .font(.system(size: 40))
+                        .border(.black)
+                        .padding(.bottom, 10)
+                    
+                    SecureField(" Password", text: $password)
+                        .font(.system(size: 40))
+                        .border(.black)
+                    
+                    if failedLogin {
+                        Text("Invalid login, please try again.")
+                            .font(.system(size: 30))
+                            .foregroundColor(.red)
+                    }
+                    
+                    Button(action: { login() }) {
+                        Image(systemName: "arrow.right.square.fill")
+                            .foregroundColor(.blue)
+                            .font(.system(size: 60))
+                            .padding(.top, 20)
+                    }
                 }
+                    .frame(width: 400, height: 200)
+                    .background(.white)
+            }.navigationViewStyle(StackNavigationViewStyle())
+        }
+    
+    }
+    
+    // Verify login with Firebase
+    // From https://designcode.io/swiftui-advanced-handbook-firebase-auth
+    func login() {
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if error != nil {
+                print(error?.localizedDescription ?? "") // debugging purposes
+                failedLogin = true
+            } else {
+                print("success") // debugging purposes
+                isLoggedIn = true
             }
-                .frame(width: 400, height: 200)
-                .background(.white)
-        }.navigationViewStyle(StackNavigationViewStyle())
+        }
     }
 }
 
