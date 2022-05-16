e11a7295db3cc10ba3a1c1c19f4a6eed03de7f3c
PenTest-duck <Yoo_Ch@student.kings.edu.au>
2022-05-17 08:43:19 +1000

    OrderModel for consistency

---
+++ AproPOS UI/Models/OrderModel.swift
@@ -0,0 +1,15 @@
+//
+//  OrderModel.swift
+//  AproPOS UI
+//
+//  Created by Chris Yoo on 12/5/22.
+//
+
+import Foundation
+
+struct OrderModel {
+    let tableNumber: Int
+    let startTime: Int
+    let status: String
+    
+}
